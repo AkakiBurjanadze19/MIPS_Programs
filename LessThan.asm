040465cae6ad6d5a747a2b7c985b9e04433e27d2
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2025-03-09 01:04:48 +0400

    add LessThan.asm

---
+++ LessThan.asm
@@ -0,0 +1,24 @@
+.data
+	num1: .word 5
+	num2: .word 10
+	message: .asciiz "Less Than"
+
+.text
+main:
+	lw $t0, num1
+	lw $t1, num2
+	
+	# check if $t0 < $t1
+	blt $t0, $t1, print_msg
+	
+	j exit
+	
+print_msg:
+	li $v0, 4
+	la $a0, message
+	syscall
+	
+exit:
+	#exit from program
+	li $v0, 10
+	syscall
